6852e6042e0f3b56323a20742aec8a4605091933
DaoGad <71476540+DaoGad@users.noreply.github.com>
2021-01-26 16:29:59 +0800

    2021.1.26 上传前端资源

---
+++ src/api/edu/teacher.js
@@ -0,0 +1,37 @@
+import request from '@/utils/request'
+
+export default{
+    getTeacherListPage(current,size,teacherQuery){
+        return request({
+            url: `/eduservice/teacher/pageTeacherCondition/${current}/${size}`,
+            method: 'post',
+            data: teacherQuery
+        })
+    },
+    deleteTeacherId(id){
+        return request({
+            url: `/eduservice/teacher/delete/${id}`,
+            method: 'delete'
+        })
+    },
+    addTeacher(teacher){
+        return request({
+            url: `/eduservice/teacher/addTeacher`,
+            method: 'post',
+            data: teacher
+        })
+    },
+    getTeacherInfo(id){
+        return request({
+            url: `/eduservice/teacher/getTeacher/${id}`,
+            method: 'get'
+        })
+    },
+    updateTeacherInfo(teacher){
+        return request({
+            url: `/eduservice/teacher/updateTeacher`,
+            method: 'post',
+            data: teacher
+        })
+}
+}
